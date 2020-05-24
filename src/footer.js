ff5818ccfa087b04db0c3630110a52af522fb205
Safal Mahat <safalmahat5@gmail.com>
2020-05-24 18:36:14 +0545

    First Commit

---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Footer(){
+    return(
+        <div>
+        <footer>
+             <p>Posted by: Hege Refsnes</p>
+        </footer>
+        </div>
+        )
+}
+
+export default Footer
